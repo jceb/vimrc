b56822b77d1f592fb46f53d1d3331c187f1972bd
Jan Christoph Ebersbach <jceb@e-jc.de>
2016-10-29 12:35:57 +0200

    Update language tool

--- pack/20_plugins/opt/LanguageTool/download_LanguageTool.sh
+++ pack/20_plugins/opt/LanguageTool/download_LanguageTool.sh
@@ -4,4 +4,4 @@
 [ -z "$(which unzip)" ] && echo "Please install unzip to run this tool." && exit 1
 
 # change this URI for newer versions of LanguageTool
-curl -O https://languagetool.org/download/LanguageTool-3.3.zip && unzip LanguageTool-3.3.zip && rm LanguageTool-3.3.zip
+curl -O https://languagetool.org/download/LanguageTool-3.5.zip && unzip LanguageTool-3.5.zip && rm LanguageTool-3.5.zip
