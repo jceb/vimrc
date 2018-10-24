2d61de224a9d954626cd43ed9f79676e88b9508d
Jan Christoph Ebersbach <jan-christoph.ebersbach@chainstep.com>
2018-10-24 07:35:35 +0200

    Update LanguageTool

--- pack/vimscripts/opt/LanguageTool/download_LanguageTool.sh
+++ pack/vimscripts/opt/LanguageTool/download_LanguageTool.sh
@@ -4,5 +4,8 @@
 [ -z "$(which unzip)" ] && echo "Please install unzip to run this tool." && exit 1
 
 # change this URI for newer versions of LanguageTool
-curl -O https://languagetool.org/download/LanguageTool-4.1.zip && unzip LanguageTool-4.1.zip && rm LanguageTool-4.1.zip
-ln -s LanguageTool-4.1 LanguageTool
+if [ -e LanguageTool ]; then
+    rm LanguageTool
+fi
+curl -O https://languagetool.org/download/LanguageTool-4.3.zip && unzip LanguageTool-4.3.zip && rm LanguageTool-4.3.zip
+ln -s LanguageTool-4.3 LanguageTool
