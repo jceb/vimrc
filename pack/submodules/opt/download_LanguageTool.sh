3602337872264854e502bf2647064a3ff33e38a0
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-04-02 13:05:02 +0200

    chore: retrieve vimscripts plugins from github

--- pack/submodules/opt/download_LanguageTool.sh
+++ pack/submodules/opt/download_LanguageTool.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
+set -u
+set -e
+
 # download language tool
 [ -z "$(which curl)" ] && echo "Please install curl to run this tool." && exit 1
 [ -z "$(which unzip)" ] && echo "Please install unzip to run this tool." && exit 1
 
+cd LanguageTool
+
 # change this URI for newer versions of LanguageTool
-if [ -e LanguageTool ]; then
+if [ -e LanguageTool ] || [ -e LanguageTool-5.2 ]; then
     rm LanguageTool
+    rm -rf LanguageTool-5.2
 fi
 curl -O https://languagetool.org/download/LanguageTool-5.2.zip && unzip LanguageTool-5.2.zip && rm LanguageTool-5.2.zip
 ln -s LanguageTool-5.2 LanguageTool
