c082c1bd6507812bf94753e6318830c4d6080ba7
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-09-14 19:30:02 +0200

    fix(snippets): correct vi modline snippet

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1793,6 +1793,47 @@ return function(use)
                 end
             end
 
+            local ls = require("luasnip")
+            local s = ls.snippet
+            -- local sn = ls.snippet_node
+            -- local t = ls.text_node
+            -- local i = ls.insert_node
+            local f = ls.function_node
+            -- local c = ls.choice_node
+            -- local d = ls.dynamic_node
+            -- local r = ls.restore_node
+            -- local l = require("luasnip.extras").lambda
+            -- local rep = require("luasnip.extras").rep
+            -- local p = require("luasnip.extras").partial
+            -- local m = require("luasnip.extras").match
+            -- local n = require("luasnip.extras").nonempty
+            -- local dl = require("luasnip.extras").dynamic_lambda
+            -- local fmt = require("luasnip.extras.fmt").fmt
+            -- local fmta = require("luasnip.extras.fmt").fmta
+            -- local types = require("luasnip.util.types")
+            -- local conds = require("luasnip.extras.conditions")
+            -- local conds_expand = require("luasnip.extras.conditions.expand")
+
+            ls.add_snippets("all", {
+                -- vi modline snippet
+                s("vi", {
+                    f(function(_, _, _)
+                        return string.format(
+                            vim.o.commentstring,
+                            string.format(
+                                "vi ft=%s:tw=%d:sw=%d:ts=%d:sts=%d:%s",
+                                vim.o.filetype,
+                                vim.o.textwidth,
+                                vim.o.shiftwidth,
+                                vim.o.tabstop,
+                                vim.o.softtabstop,
+                                vim.o.expandtab and "et" or "noet"
+                            )
+                        )
+                    end, {}, { user_args = {} }),
+                }),
+            })
+
             vim.cmd([[
                 augroup choice_popup
                 au!
