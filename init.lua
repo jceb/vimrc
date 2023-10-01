2ba79322db894ea79c8e15721181904a59bf039e
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-10-01 16:59:58 +0200

    chore: cleanup init

--- init.lua
+++ init.lua
@@ -9,6 +9,7 @@ vim.cmd.packadd("myconfig_1_pre")
 require("settings")
 require("keybindings")
 
+-- https://github.com/folke/lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
@@ -21,7 +22,6 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
--- https://github.com/folke/lazy.nvim
 require("lazy").setup("plugins", {
     root = vim.fn.stdpath("config") .. "/lazy",
     ui = { custom_keys = {
@@ -43,39 +43,34 @@ require("lazy").setup("plugins", {
     },
 })
 
--- load matchup plugin before matchit
--- packadd matchup
-
--- function! s:init()
 vim.opt.packpath:append(vim.fn.stdpath("config"))
--- " personal vim settings
+-- personal vim settings
 vim.cmd.packadd("myconfig_2_post")
 
--- " workaround because the event isn't triggered by the above command for some
--- " unknown reason
+-- workaround because the event isn't triggered by the above command for some
+-- unknown reason
 vim.cmd.doau("ColorScheme")
 
--- let g:my_gui_font = "JetBrainsMono Nerd Font:h9"
--- exec ":set guifont=".fnameescape(g:my_gui_font)
--- command! GuiFontBigger  :exec ":set guifont=".fnameescape(substitute(&guifont, '\d\+$', '\=submatch(0)+1', ''))
--- command! GuiFontSmaller :exec ":set guifont=".fnameescape(substitute(&guifont, '\d\+$', '\=submatch(0)-1', ''))
--- nnoremap <silent> <C-0> :<C-u>exec ":set guifont=".fnameescape(g:my_gui_font)<CR>
--- nnoremap <silent> <C--> :<C-u>GuiFontSmaller<CR>
--- nnoremap <silent> <C-8> :<C-u>GuiFontSmaller<CR>
--- nnoremap <silent> <C-ScrollWheelDown> :<C-u>GuiFontSmaller<CR>
--- nnoremap <silent> <C-=> :<C-u>GuiFontBigger<CR>
--- nnoremap <silent> <C-+> :<C-u>GuiFontBigger<CR>
--- nnoremap <silent> <C-9> :<C-u>GuiFontBigger<CR>
--- nnoremap <silent> <C-ScrollWheelUp> :<C-u>GuiFontBigger<CR>
+vim.g.my_gui_font = "JetBrainsMono Nerd Font:h9"
+vim.o.guifont = vim.fn.fnameescape(vim.g.my_gui_font)
+vim.cmd([[
+command! GuiFontBigger  :exec ":set guifont=".fnameescape(substitute(&guifont, '\d\+$', '\=submatch(0)+1', ''))
+command! GuiFontSmaller :exec ":set guifont=".fnameescape(substitute(&guifont, '\d\+$', '\=submatch(0)-1', ''))
+]])
+
+map = vim.api.nvim_set_keymap
+unmap = vim.api.nvim_del_keymap
+
+map("n", "<C-0>", ":<C-u>exec ':set guifont='.fnameescape(g:my_gui_font)<CR>", { silent = true })
+map("n", "<C-->", ":<C-u>GuiFontSmaller<CR>", { silent = true })
+map("n", "<C-8>", ":<C-u>GuiFontSmaller<CR>", { silent = true })
+map("n", "<C-ScrollWheelDown>", ":<C-u>GuiFontSmaller<CR>", { silent = true })
+map("n", "<C-=>", ":<C-u>GuiFontBigger<CR>", { silent = true })
+map("n", "<C-+>", ":<C-u>GuiFontBigger<CR>", { silent = true })
+map("n", "<C-9>", ":<C-u>GuiFontBigger<CR>", { silent = true })
+map("n", "<C-ScrollWheelUp>", ":<C-u>GuiFontBigger<CR>", { silent = true })
 
 -- " set SSH environment variable in case it isn't set, e.g. in nvim-qt
 -- if getenv('SSH_AUTH_SOCK') == v:null
 --   call setenv('SSH_AUTH_SOCK', systemlist('gpgconf --list-dirs agent-ssh-socket')[0])
--- endif
--- endfunction
-
--- if v:vim_did_enter
---   call s:init()
--- else
---   au VimEnter * call s:init()
 -- endif
