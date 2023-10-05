c6ccb057ae0700292335f333424c943b828f069c
Jan Christoph Ebersbach <jceb@e-jc.de>
2023-10-05 23:34:31 +0200

    fix(ftplugins): nushell issue

--- lua/plugins/ftplugins.lua
+++ lua/plugins/ftplugins.lua
@@ -188,7 +188,7 @@ return {
     {
         -- https://github.com/iamcco/markdown-preview.nvim
         "iamcco/markdown-preview.nvim",
-        build = { "cd app && yarn install" },
+        build = { "cd app; yarn install" },
         lazy = true,
         ft = { "markdown" },
         config = function()
