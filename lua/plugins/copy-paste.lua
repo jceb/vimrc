7d1e39023f9b641c1d987cc48c8aec02c0694b5c
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-03-02 16:14:26 +0100

    chore: reindent plugin

--- lua/plugins/copy-paste.lua
+++ lua/plugins/copy-paste.lua
@@ -2,27 +2,27 @@ map = vim.api.nvim_set_keymap
 unmap = vim.api.nvim_del_keymap
 
 return {
-    ----------------------
-    -- copy / paste
-    ----------------------
-    {
-        -- https://github.com/svermeulen/vim-yoink
-        "svermeulen/vim-yoink",
-        lazy = true,
-        keys = {
-            { "p" },
-            { "P" },
-            { "<M-n>" },
-            { "<M-p>" },
-        },
-        config = function()
-            vim.g.yoinkAutoFormatPaste = 0 -- this doesn't work properly, so fix it to <F11> manualy
-            vim.g.yoinkMaxItems = 20
-
-            map("n", "<M-n>", "<plug>(YoinkPostPasteSwapBack)", {})
-            map("n", "<M-p>", "<plug>(YoinkPostPasteSwapForward)", {})
-            map("n", "p", "<plug>(YoinkPaste_p)", {})
-            map("n", "P", "<plug>(YoinkPaste_P)", {})
-        end,
+  ----------------------
+  -- copy / paste
+  ----------------------
+  {
+    -- https://github.com/svermeulen/vim-yoink
+    "svermeulen/vim-yoink",
+    lazy = true,
+    keys = {
+      { "p" },
+      { "P" },
+      { "<M-n>" },
+      { "<M-p>" },
     },
+    config = function()
+      vim.g.yoinkAutoFormatPaste = 0       -- this doesn't work properly, so fix it to <F11> manualy
+      vim.g.yoinkMaxItems = 20
+
+      map("n", "<M-n>", "<plug>(YoinkPostPasteSwapBack)", {})
+      map("n", "<M-p>", "<plug>(YoinkPostPasteSwapForward)", {})
+      map("n", "p", "<plug>(YoinkPaste_p)", {})
+      map("n", "P", "<plug>(YoinkPaste_P)", {})
+    end,
+  },
 }
