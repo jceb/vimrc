a3263824147da2228e0b1a359109193be8c05f30
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-03-26 07:21:29 +0100

    chore: increase text width and disable window navigation mappings

--- init.lua
+++ init.lua
@@ -220,10 +220,10 @@ vim.opt.report = 0 -- report every changed line
 vim.opt.clipboard:remove({ "autoselect" }) -- disable itegration with X11 clipboard
 vim.opt.virtualedit = "block,onemore"      -- allow the cursor to move beyond the last character of a line
 vim.opt.copyindent = true                  -- always copy indentation level from previous line
-vim.opt.textwidth = 80                     -- default textwidth
+vim.opt.textwidth = 120                    -- default textwidth
 vim.opt.shiftwidth = 2                     -- number of spaces to use for each step of indent
-vim.opt.tabstop = 4                        -- number of spaces a tab counts for
-vim.opt.softtabstop = 4                    -- number of spaces a tab counts for
+vim.opt.tabstop = 2                        -- number of spaces a tab counts for
+vim.opt.softtabstop = 2                    -- number of spaces a tab counts for
 vim.opt.expandtab = true                   -- insert tabs instead of spaces
 vim.opt.formatoptions = "crqj"             -- no automatic linebreak, no whatsoever expansion
 vim.opt.pastetoggle = "<F1>"               -- put vim in pastemode - usefull for pasting in console-mode
@@ -238,28 +238,22 @@ vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagn
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
---
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+-- -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- -- is not what someone will guess without a bit more experience.
+-- --
+-- -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- -- or just use <C-\><C-n> to exit terminal mode
+-- vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-
--- Keybinds to make split navigation easier.
---  Use CTRL+<hjkl> to switch between windows
---
---  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+-- -- Keybinds to make split navigation easier.
+-- --  Use CTRL+<hjkl> to switch between windows
+-- --
+-- --  See `:help wincmd` for a list of all window commands
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
