d6f51493a0b4b65ae6759509681c2512872cd676
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-04-02 06:57:51 +0200

    chore: reactivate trouble.nvim

--- lua/plugins/development.lua
+++ lua/plugins/development.lua
@@ -73,17 +73,44 @@ return {
   --         }
   --     end,
   -- },
-  -- {
-  --     -- https://github.com/folke/trouble.nvim
-  --     "folke/trouble.nvim",
-  --     dependencies = {
-  --         -- https://github.com/kyazdani42/nvim-web-devicons
-  --         "nvim-tree/nvim-web-devicons",
-  --     },
-  --     config = function()
-  --         require("trouble").setup({})
-  --     end,
-  -- },
+  {
+    -- https://github.com/folke/trouble.nvim
+    "folke/trouble.nvim",
+    branch = "dev",
+    cmd = { "Trouble" },
+    keys = {
+      {
+        "<leader>,,",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      -- {
+      --   "<leader>xX",
+      --   "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      --   desc = "Buffer Diagnostics (Trouble)",
+      -- },
+      -- {
+      --   "<leader>cs",
+      --   "<cmd>Trouble symbols toggle focus=false<cr>",
+      --   desc = "Symbols (Trouble)",
+      -- },
+      -- {
+      --   "<leader>cl",
+      --   "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      --   desc = "LSP Definitions / references / ... (Trouble)",
+      -- },
+      {
+        "<leader>,l",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>,l",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
   {
     -- https://github.com/dbeniamine/cheat.sh-vim
     "dbeniamine/cheat.sh-vim",
