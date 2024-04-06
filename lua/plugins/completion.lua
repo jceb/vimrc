7c9583a53738ab29c2f2bb60a27aec0b49aa8205
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-04-06 16:53:33 +0200

    chore(completion): optimize incompatible lsp detection

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -388,7 +388,7 @@ return {
           capabilities = capabilities,
         }
         -- Nushell doesn't support navic. Therefore, to avoid a navic error, attach lsp_format format directly
-        if server == "nushell" or server == "eslint" then
+        if vim.tbl_contains({ "nushell", "eslint" }, server) then
           opts.on_attach = lsp_format
         end
         lspconfig[server].setup(vim.tbl_deep_extend("force", opts, config))
