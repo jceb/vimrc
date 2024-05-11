98116d09b92b0a1b723badf13896929d65bff2fd
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-05-11 17:03:24 +0200

    chore(visuals): deactivate gitsigns

--- lua/plugins/visuals.lua
+++ lua/plugins/visuals.lua
@@ -434,19 +434,19 @@ return {
   --    require('gitsigns').setup({ ... })
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    -- https://github.com/lewis6991/gitsigns.nvim
-    "lewis6991/gitsigns.nvim",
-    opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-    },
-  },
+  -- { -- Adds git related signs to the gutter, as well as utilities for managing changes
+  --   -- https://github.com/lewis6991/gitsigns.nvim
+  --   "lewis6991/gitsigns.nvim",
+  --   opts = {
+  --     signs = {
+  --       add = { text = "+" },
+  --       change = { text = "~" },
+  --       delete = { text = "_" },
+  --       topdelete = { text = "‾" },
+  --       changedelete = { text = "~" },
+  --     },
+  --   },
+  -- },
 
   -- {                     -- Useful plugin to show you pending keybinds.
   --   "folke/which-key.nvim",
