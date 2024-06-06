1961578489707800a7f2da330e31787a60f17493
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-06-06 09:10:33 +0200

    chore: replace autopairs with ultimate-autopairs

--- lua/plugins/text-transformation.lua
+++ lua/plugins/text-transformation.lua
@@ -224,21 +224,62 @@ return {
   --         )
   --     end,
   -- },
+  -- {
+  --   -- https://github.com/windwp/nvim-autopairs
+  --   -- Replaced by mini.pairs
+  --   "windwp/nvim-autopairs",
+  --   keys = {
+  --     { "{", mode = "i" },
+  --     { "[", mode = "i" },
+  --     { "(", mode = "i" },
+  --     { "<", mode = "i" },
+  --     { "'", mode = "i" },
+  --     { '"', mode = "i" },
+  --   },
+  --   config = function()
+  --     require("nvim-autopairs").setup({})
+  --   end,
+  -- },
   {
-    -- https://github.com/windwp/nvim-autopairs
-    -- Replaced by mini.pairs
-    "windwp/nvim-autopairs",
-    keys = {
-      { "{", mode = "i" },
-      { "[", mode = "i" },
-      { "(", mode = "i" },
-      { "<", mode = "i" },
-      { "'", mode = "i" },
-      { '"', mode = "i" },
+    -- https://github.com/altermo/ultimate-autopair.nvim
+    'altermo/ultimate-autopair.nvim',
+    event = {
+      'InsertEnter',
+      -- 'CmdlineEnter'
     },
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
+    -- branch = 'v0.6', --recommended as each new version will have breaking changes
+    opts =
+    {
+      cmap = false,
+      tabout = {
+        enable = true
+      },
+      internal_pairs = { -- *ultimate-autopair-pairs-default-pairs*
+        { '[', ']', fly = true,     dosuround = true, newline = true, space = true },
+        { '(', ')', fly = true,     dosuround = true, newline = true, space = true },
+        { '{', '}', fly = true,     dosuround = true, newline = true, space = true },
+        { '"', '"', suround = true, multiline = false },
+        {
+          "'",
+          "'",
+          suround = true,
+          cond = function(fn)
+            return not
+                fn.in_lisp() or fn.in_string()
+          end,
+          alpha = true,
+          nft = { 'tex' },
+          multiline = false
+        },
+        { '`',    '`',   cond = function(fn) return not fn.in_lisp() or fn.in_string() end, nft = { 'tex' },    multiline = false },
+        { '``',   "''",  ft = { 'tex' } },
+        { '```',  '```', newline = true,                                                    ft = { 'markdown' } },
+        { '<!--', '-->', ft = { 'markdown', 'html' },                                       space = true },
+        { '"""',  '"""', newline = true,                                                    ft = { 'python' } },
+        { "'''",  "'''", newline = true,                                                    ft = { 'python' } },
+        { "<",    ">",   newline = true,                                                    dosuround = true,   ft = { 'html', 'xml', 'xsl', 'xslt', 'javascriptreact', 'typescriptreact' } },
+      },
+    },
   },
   {
     -- https://github.com/junegunn/vim-easy-align
