290b677e446e62da2c4ba6834ddacad2590f9b2c
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-06-08 17:18:07 +0200

    feat(text-transformation): add ai.nvim plugin

--- lua/plugins/text-transformation.lua
+++ lua/plugins/text-transformation.lua
@@ -24,6 +24,67 @@ return {
   --     end,
   -- },
   {
+    -- https://github.com/gera2ld/ai.nvim
+    'gera2ld/ai.nvim',
+    dependencies = 'nvim-lua/plenary.nvim',
+    keys = {
+      { "<leader>ai", "<cmd>AIImprove<CR>",   mode = "x" },
+      { "<leader>ai", ":AIImprove ",          mode = "n" },
+      { "<leader>aa", "<cmd>AIAsk<CR>",       mode = "x" },
+      { "<leader>aa", ":AIAsk ",              mode = "n" },
+      { "<leader>ad", "<cmd>AIDefine<CR>",    mode = "x" },
+      { "<leader>ad", ":AIDefine ",           mode = "n" },
+      { "<leader>at", "<cmd>AITranslate<CR>", mode = "x" },
+      { "<leader>at", ":AITranslate ",        mode = "n" },
+    },
+    opts = {
+      ---- AI's answer is displayed in a popup buffer
+      ---- Default behaviour is not to give it the focus because it is seen as a kind of tooltip
+      ---- But if you prefer it to get the focus, set to true.
+      result_popup_gets_focus = true,
+      ---- Override default prompts here, see below for more details
+      -- prompts = {},
+      ---- Default models for each prompt, can be overridden in the prompt definition
+      models = {
+        -- {
+        --   provider = 'gemini',
+        --   model = 'gemini-1.5-flash',
+        --   result_tpl = '## Gemini\n\n{{output}}',
+        -- },
+        {
+          provider = 'gemini',
+          model = 'gemini-pro',
+          result_tpl = '## Gemini\n\n{{output}}',
+        },
+        {
+          provider = 'openai',
+          model = 'gpt-3.5-turbo',
+          result_tpl = '## GPT-3.5\n\n{{output}}',
+        },
+      },
+
+      --- API keys and relavant config
+      gemini = {
+        -- api_key = 'YOUR_GEMINI_API_KEY',
+        model = 'gemini-pro',
+        -- proxy = '',
+      },
+      -- openai = {
+      --   api_key = 'YOUR_OPENAI_API_KEY',
+      --   -- base_url = 'https://api.openai.com/v1',
+      --   -- model = 'gpt-4',
+      --   -- proxy = '',
+      -- },
+    },
+    config = function()
+      local ai = require('ai')
+      local ok, opts = pcall(vim.fn.json_decode, os.getenv('AI_NVIM_PROVIDER_CONFIG'))
+      opts = ok and opts or {}
+      ai.setup(opts)
+    end
+    event = 'VeryLazy',
+  },
+  {
     -- https://github.com/andrewferrier/debugprint.nvim
     "andrewferrier/debugprint.nvim",
     opts = {
