32230547ac1f26ef26cc2ba79a588d1d9df80668
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-06-21 08:24:41 +0200

    fix(development): add missing rest.nvim dependencies

--- lua/plugins/development.lua
+++ lua/plugins/development.lua
@@ -181,14 +181,21 @@ return {
     dependencies = {
       -- https://github.com/nvim-lua/plenary.nvim
       "nvim-lua/plenary.nvim",
+      {
+        -- https://github.com/vhyrro/luarocks.nvim
+        "vhyrro/luarocks.nvim",
+        priority = 1000,
+        config = true,
+        opts = {
+          rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" }
+        }
+      },
     },
     -- FIXMED: opt doesn't seem to work
     lazy = true,
     ft = { "http" },
     config = function()
-      require("rest-nvim").setup({
-        result_split_horizontal = false,
-      })
+      require("rest-nvim").setup()
       vim.cmd([[
         au FileType http nmap <buffer> <silent> <C-j> <Plug>RestNvim
         au FileType http nmap <buffer> <silent> <C-q> <Plug>RestNvimPreview
