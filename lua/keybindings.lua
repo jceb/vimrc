4e8003d5381b711529d9f2dffa1ee73fa995851f
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-11-23 21:36:22 +0100

    chore(keybindings): add fast window movement mappings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -46,6 +46,11 @@ function! Yank(type, ...)
 endfunction
 ]])
 
+map("n", "<C-h>", "<C-w>h", { noremap = true })
+map("n", "<C-j>", "<C-w>j", { noremap = true })
+map("n", "<C-k>", "<C-w>k", { noremap = true })
+map("n", "<C-l>", "<C-w>l", { noremap = true })
+
 map("n", "gy", ":<C-u>set opfunc=Yank<CR>g@", {
   silent = true,
   noremap = true,
