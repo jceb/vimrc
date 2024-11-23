69d7e989fcde092e20b79d15c5ea146f5227b21e
Jan Christoph Ebersbach <jceb@e-jc.de>
2024-11-23 17:04:02 +0100

    feat(neoterm): add window movement bindings to terminals

--- lua/plugins/custom/neoterm.lua
+++ lua/plugins/custom/neoterm.lua
@@ -33,6 +33,16 @@ return {
     vim.g.neoterm_shell = "nu"
     vim.g.neoterm_autoinsert = 1
     vim.g.neoterm_automap_keys = "<F23>"
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 }
+      -- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+      vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+    end
+    vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
   end,
   config = function()
     -- taken from https://github.com/folke/lazy.nvim/issues/574
