68e64df79e46efb241468fab2228a6086f6be28e
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-01-13 21:06:56 +0100

    fix(colors): increase day brightness of colors

--- colors/mytokyonight.lua
+++ colors/mytokyonight.lua
@@ -1,5 +1,6 @@
 package.loaded["tokyonight.colors"] = nil
 require("tokyonight").load({
-    transparent = not vim.g.neovide, -- Enable this to disable setting the background color
-    style = vim.g.tokyonight_style or (vim.o.background == "light" and "day") or nil,
+  transparent = not vim.g.neovide, -- Enable this to disable setting the background color
+  style = vim.g.tokyonight_style or (vim.o.background == "light" and "day") or nil,
+  day_brightness = 0.5,
 })
