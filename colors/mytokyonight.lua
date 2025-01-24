2cabf5c0cea48956f02a3788c9dafb7cf22eed2c
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-01-24 08:11:11 +0100

    chore(mytokyonight): decrease day brightness

--- colors/mytokyonight.lua
+++ colors/mytokyonight.lua
@@ -2,5 +2,5 @@ package.loaded["tokyonight.colors"] = nil
 require("tokyonight").load({
   transparent = not vim.g.neovide, -- Enable this to disable setting the background color
   style = vim.g.tokyonight_style or (vim.o.background == "light" and "day") or nil,
-  day_brightness = 0.5,
+  day_brightness = 0.4,
 })
