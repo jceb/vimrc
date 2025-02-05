965a29f042b8a079f8ce88e77799176fd7bc267d
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-02-05 15:55:21 +0100

    chore(hop): change default jump character to S

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -6,8 +6,8 @@ return {
     keys = "etovxqpdygfblzhckisuran",
   },
   keys = {
-    { "<CR>", "<cmd>HopChar1<cr>" },
-    { "<CR>", "<cmd>HopChar1<cr>", { mode = "o" } },
+    { "S", "<cmd>HopChar1<cr>" },
+    { "S", "<cmd>HopChar1<cr>", { mode = "o" } },
     -- { "s", "<cmd>HopChar1AC<cr>" },
     -- { "S", "<cmd>HopChar1BC<cr>" },
     -- { "s", "<cmd>HopChar1AC<cr>", { mode = "o" } },
