a44d6790e2b0c88812ea7e72ca4c42d8194b4a9c
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-04-04 08:25:25 +0200

    fix(blink): disable c-e mapping in command mode

--- lua/custom/plugins/blink.lua
+++ lua/custom/plugins/blink.lua
@@ -302,7 +302,14 @@ return {
     -- C-k: Toggle signature help
     --
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default", ["<C-e>"] = {} },
+    keymap = { preset = "default" },
+    cmdline = {
+      keymap = {
+        preset = "cmdline",
+        -- disable blink keymap in favor of rsi.vim
+        ["<C-e>"] = {},
+      },
+    },
     snippets = { preset = "luasnip" },
 
     appearance = {
