a20bb2c64058b2fc4a06a0ddf8e525f495b9f148
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-05-03 15:56:41 +0200

    chore(codecompanion): make codecompanion load lazily

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,8 +1,18 @@
 return {
+  -- https://github.com/olimorris/codecompanion.nvim
   "olimorris/codecompanion.nvim",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
+  },
+  keys = {
+    { "<Leader>A", "<cmd>CodeCompanionActions<cr>", { mode = { "n", "v" }, noremap = true, silent = true } },
+    { "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { mode = { "n", "v" }, noremap = true, silent = true } },
+    { "ga", "<cmd>CodeCompanionChat Add<cr>", { mode = "v", noremap = true, silent = true } },
+  },
+  cmd = {
+    "CodeCompanionActions",
+    "CodeCompanionChat",
   },
   config = function()
     require("codecompanion").setup({
@@ -17,9 +27,9 @@ return {
         },
       },
     })
-    vim.keymap.set({ "n", "v" }, "<Leader>A", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
-    vim.keymap.set({ "n", "v" }, "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
-    vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+    -- vim.keymap.set({ "n", "v" }, "<Leader>A", "<cmd>CodeCompanionActions<cr>", { noremap = true, silent = true })
+    -- vim.keymap.set({ "n", "v" }, "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+    -- vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
 
     -- Expand 'cc' into 'CodeCompanion' in the command line
     vim.cmd([[cab cc CodeCompanion]])
