107354a0b5bf91e5710a5618fbcfd22d6e237c6e
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-05-03 15:54:38 +0200

    chore(vim-rsi): add more rsi bindings

--- lua/custom/plugins/vim-rsi.lua
+++ lua/custom/plugins/vim-rsi.lua
@@ -3,5 +3,7 @@ return {
   "tpope/vim-rsi",
   init = function()
     vim.g.rsi_no_meta = 1
+    vim.keymap.set("c", "<M-b>", "<C-Left>", { noremap = true })
+    vim.keymap.set("c", "<M-f>", "<C-Right>", { noremap = true })
   end,
 }
