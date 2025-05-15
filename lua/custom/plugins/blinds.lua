e763b39e658cc6d50115954f496340592899e2f6
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-05-15 14:42:20 +0200

    chore: update blinds and add dotenv configuration

--- lua/custom/plugins/blinds.lua
+++ lua/custom/plugins/blinds.lua
@@ -1,8 +1,11 @@
 return {
   -- https://github.com/jceb/blinds.nvim
   "jceb/blinds.nvim",
-  init = function()
-    vim.g.blinds_guibg = "#cdcdcd"
-  end,
-  config = true,
+  -- init = function()
+  --   vim.g.blinds_guibg = "#cdcdcd"
+  -- end,
+  -- config = true,
+  config = {
+    guibg = "#cdcdcd",
+  },
 }
