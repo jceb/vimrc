d708872a738880ed4a4a399ac18bc68b9ede8067
Jan Christoph Ebersbach <jceb@e-jc.de>
2025-07-16 09:48:53 +0200

    chore(rest): update plugin

--- lua/custom/plugins/rest.lua
+++ lua/custom/plugins/rest.lua
@@ -2,17 +2,24 @@ return {
   -- https://github.com/NTBBloodbath/rest.nvim
   "NTBBloodbath/rest.nvim",
   dependencies = {
-    -- https://github.com/nvim-lua/plenary.nvim
-    "nvim-lua/plenary.nvim",
-    {
-      -- https://github.com/vhyrro/luarocks.nvim
-      "vhyrro/luarocks.nvim",
-      priority = 1000,
-      config = true,
-      opts = {
-        rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
-      },
-    },
+    -- https://github.com/nvim-neotest/nvim-nio
+    "nvim-neotest/nvim-nio",
+    -- -- https://github.com/nvim-lua/plenary.nvim
+    -- "nvim-lua/plenary.nvim",
+    -- {
+    --   -- https://github.com/vhyrro/luarocks.nvim
+    --   "vhyrro/luarocks.nvim",
+    --   priority = 1000,
+    --   config = true,
+    --   opts = {
+    --     rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+    --   },
+    -- },
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      table.insert(opts.ensure_installed, "http")
+    end,
   },
   -- FIXMED: opt doesn't seem to work
   ft = { "http" },
