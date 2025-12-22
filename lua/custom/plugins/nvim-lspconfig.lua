4a2b86e11e57393d71bcf758220f11f9c44f3499
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-12-22 11:21:36 +0100

    feat: map lsp import completion to <C-x>

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -92,6 +92,13 @@ return {
         --  For example, in C this would take you to the header
         map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+        --  Get completion information
+        -- Use CTRL-x to trigger LSP completion.
+        -- Use CTRL-Y to select an item. |complete_CTRL-Y|
+        vim.keymap.set("i", "<C-x>", function()
+          vim.lsp.completion.get()
+        end, { buffer = event.buf, desc = "LSP: Get completion" })
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         --    See `:help CursorHold` for information about when this is executed
