9c68d44b9bde060f39625dd4b3075f276edad117
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-01-01 07:59:49 +0100

    chore(nvim-lspconfig): improve configuration

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -297,8 +297,9 @@ return {
               -- Tells lua_ls where to find all the Lua files that you have loaded
               -- for your neovim configuration.
               library = {
-                "${3rd}/luv/library",
-                unpack(vim.api.nvim_get_runtime_file("", true)),
+                -- "${3rd}/luv/library",
+                -- unpack(vim.api.nvim_get_runtime_file("", true)),
+                vim.api.nvim_get_runtime_file("", true),
               },
               -- If lua_ls is really slow on your computer, you can try this instead:
               -- library = { vim.env.VIMRUNTIME },
@@ -306,14 +307,14 @@ return {
             completion = {
               callSnippet = "Replace",
             },
-            -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
             diagnostics = {
+              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+              -- disable = { "missing-fields" },
               -- Get the language server to recognize the `vim` global
               globals = { "vim" },
             },
-            -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
+              -- Do not send telemetry data containing a randomized but unique identifier
               enable = false,
             },
           },
