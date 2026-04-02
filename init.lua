088ed4e580c65e531f1f30798ef3b9bc657c18cc
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-04-02 10:29:24 +0200

    feat: enable floating cmdline and message window

--- init.lua
+++ init.lua
@@ -287,6 +287,56 @@ vim.diagnostic.config({
   -- virtual_lines = { current_line = true },
 })
 
+-- Source: https://www.reddit.com/r/neovim/comments/1sa95g4/no_more_press_enter_with_ui2_with_example/
+-- Experimental UI2: floating cmdline and messages
+vim.o.cmdheight = 0
+require("vim._core.ui2").enable({
+  enable = true,
+  msg = {
+    targets = {
+      [""] = "msg",
+      empty = "cmd",
+      bufwrite = "msg",
+      confirm = "cmd",
+      emsg = "pager",
+      echo = "msg",
+      echomsg = "msg",
+      echoerr = "pager",
+      completion = "cmd",
+      list_cmd = "pager",
+      lua_error = "pager",
+      lua_print = "msg",
+      progress = "pager",
+      rpc_error = "pager",
+      quickfix = "msg",
+      search_cmd = "cmd",
+      search_count = "cmd",
+      shell_cmd = "pager",
+      shell_err = "pager",
+      shell_out = "pager",
+      shell_ret = "msg",
+      undo = "msg",
+      verbose = "pager",
+      wildlist = "cmd",
+      wmsg = "msg",
+      typed_cmd = "cmd",
+    },
+    cmd = {
+      height = 0.5,
+    },
+    dialog = {
+      height = 0.5,
+    },
+    msg = {
+      height = 0.3,
+      timeout = 5000,
+    },
+    pager = {
+      height = 0.5,
+    },
+  },
+})
+
 require("lazy").setup({
   ----------------------
   -- buffer management {{{1
