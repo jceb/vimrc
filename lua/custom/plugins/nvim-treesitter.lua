150351ff09769a09ec5605cbc3a366e4c2137556
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2026-04-02 16:12:37 +0200

    fix(nvim-treesitter): update nvim-treesitter and disable ts_context_commentstring

--- lua/custom/plugins/nvim-treesitter.lua
+++ lua/custom/plugins/nvim-treesitter.lua
@@ -2,9 +2,10 @@ return {
   -- https://github.com/nvim-treesitter/nvim-treesitter
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  branch = "main",
   dependencies = {
     -- https://github.com/JoosepAlviste/nvim-ts-context-commentstring
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    -- "JoosepAlviste/nvim-ts-context-commentstring",
     -- -- https://github.com/nvim-treesitter/nvim-treesitter-context
     -- "nvim-treesitter/nvim-treesitter-context",
     -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects
@@ -16,74 +17,96 @@ return {
     -- require C++ others C
     -- require("nvim-treesitter.install").compilers = { "clang" }
     -- require("nvim-treesitter.install").compilers = { "clang++", "clang" }
-    vim.g.skip_ts_context_commentstring_module = true
-    require("ts_context_commentstring").setup({})
+    -- vim.g.skip_ts_context_commentstring_module = true
+    -- require("ts_context_commentstring").setup({})
     ---@diagnostic disable-next-line: missing-fields
     -- require("nvim-treesitter.install").prefer_git = true
-    require("nvim-treesitter.configs").setup({
-      -- integration with ./vim-matchup.lua plugin
-      matchup = {
-        enable = true, -- mandatory, false will disable the whole extension
-        -- disable = { "c", "ruby" }, -- optional, list of language that will be disabled
-      },
-      ensure_installed = {
-        "bash",
-        "c",
-        "go",
-        "html",
-        "javascript",
-        "lua",
-        "markdown",
-        -- "nu",
-        "python",
-        "rust",
-        "typescript",
-        "vim",
-        "vimdoc",
-      },
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        -- Good introduction https://pawelgrzybek.com/nvim-incremental-selection/
-        enable = true,
-        keymaps = {
-          init_selection = "<M-o>",
-          node_incremental = "<M-o>",
-          scope_incremental = "<M-O>",
-          node_decremental = "<M-i>",
-          -- init_selection = "gnn",
-          -- node_incremental = "grn",
-          -- scope_incremental = "grc",
-          -- node_decremental = "grm",
-        },
-      },
-      -- There are additional nvim-treesitter modules that you can use to interact
-      -- with nvim-treesitter. You should go explore a few and see what interests you:
-      --
-      --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
-    })
+    require("nvim-treesitter").setup({})
+    -- require("nvim-treesitter").setup({
+    --   -- integration with ./vim-matchup.lua plugin
+    --   matchup = {
+    --     enable = true, -- mandatory, false will disable the whole extension
+    --     -- disable = { "c", "ruby" }, -- optional, list of language that will be disabled
+    --   },
+    --   ensure_installed = {
+    --     "bash",
+    --     "c",
+    --     "go",
+    --     "html",
+    --     "javascript",
+    --     "lua",
+    --     "markdown",
+    --     -- "nu",
+    --     "python",
+    --     "rust",
+    --     "typescript",
+    --     "vim",
+    --     "vimdoc",
+    --   },
+    --   auto_install = true,
+    --   highlight = { enable = true },
+    --   indent = { enable = true },
+    --   incremental_selection = {
+    --     -- Good introduction https://pawelgrzybek.com/nvim-incremental-selection/
+    --     enable = true,
+    --     keymaps = {
+    --       init_selection = "<M-o>",
+    --       node_incremental = "<M-o>",
+    --       scope_incremental = "<M-O>",
+    --       node_decremental = "<M-i>",
+    --       -- init_selection = "gnn",
+    --       -- node_incremental = "grn",
+    --       -- scope_incremental = "grc",
+    --       -- node_decremental = "grm",
+    --     },
+    --   },
+    --   -- There are additional nvim-treesitter modules that you can use to interact
+    --   -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --   --
+    --   --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --   --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --   --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    -- })
     -- Enable support for rest.nvim https://github.com/NTBBloodbath/rest.nvim
-    local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_configs.http = {
-      install_info = {
-        url = "https://github.com/NTBBloodbath/tree-sitter-http",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-    }
-    parser_configs.just = {
-      install_info = {
-        url = "https://github.com/IndianBoy42/tree-sitter-just",
-        -- url = "https://github.com/IndianBoy42/tree-sitter-just",
-        files = { "src/parser.c", "src/scanner.c" },
-        branch = "main",
-      },
-      -- filetype = "just",
-      maintainers = { "@IndianBoy42" },
-    }
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "TSUpdate",
+      callback = function()
+        require("nvim-treesitter.parsers").http = {
+          install_info = {
+            url = "https://github.com/NTBBloodbath/tree-sitter-http",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+        }
+        require("nvim-treesitter.parsers").just = {
+          install_info = {
+            url = "https://github.com/casey/tree-sitter-just",
+            -- url = "https://github.com/IndianBoy42/tree-sitter-just",
+            files = { "src/parser.c", "src/scanner.c" },
+            branch = "main",
+          },
+          -- filetype = "just",
+          maintainers = { "casey/tree-sitter-just maintainers" },
+        }
+      end,
+    })
+    require("nvim-treesitter").install({
+      "bash",
+      "c",
+      "go",
+      "html",
+      "javascript",
+      "lua",
+      "markdown",
+      -- "nu",
+      "just",
+      "http",
+      "python",
+      "rust",
+      "typescript",
+      "vim",
+      "vimdoc",
+    })
     vim.o.foldmethod = "expr"
     vim.o.foldexpr = "nvim_treesitter#foldexpr()"
   end,
