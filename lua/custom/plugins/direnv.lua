48052ebbd7454c5d63e19ba8b688ef0fafe39866
Jan Christoph Ebersbach <jceb@e-jc.de>
2026-07-10 16:00:54 +0200

    chore(direnv): disable direnv plugin and only use the syntax highlighting

--- lua/custom/plugins/direnv.lua
+++ lua/custom/plugins/direnv.lua
@@ -1,4 +1,8 @@
 return {
   -- https://github.com/direnv/direnv.vim
   "direnv/direnv.vim",
+  init = function()
+    -- disable direnv as it is too slow - just keep the syntax highlighting
+    vim.g.loaded_direnv = true
+  end,
 }
