db111e4b9661cbfd32d69344d70f6059ac545249
Jan Christoph Ebersbach <jceb@e-jc.de>
2026-07-14 10:48:04 +0200

    fix(direnv): reenable direnv partially

--- lua/custom/plugins/direnv.lua
+++ lua/custom/plugins/direnv.lua
@@ -2,7 +2,8 @@ return {
   -- https://github.com/direnv/direnv.vim
   "direnv/direnv.vim",
   init = function()
-    -- disable direnv as it is too slow - just keep the syntax highlighting
-    vim.g.loaded_direnv = true
+    -- disable direnv auto loading as it is too slow
+    -- Manually execute this command to load direnv: :DirenvExport
+    vim.g.direnv_auto = false
   end,
 }
