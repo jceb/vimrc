c50b5259b49b5460c795d234ca7c3a18f34492d4
Jan Christoph Ebersbach <jceb@e-jc.de>
2026-09-09 16:05:38 +0200

    Revert "feat: migrate to vim 0.13 and use multicursor"
    This reverts commit ea79f147b11e6425a9dd5cd1fc06b7490f5e32df.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -2,10 +2,10 @@
 -- ------------
 
 -- quick navigation between windows
-vim.keymap.set("n", "<A-h>", "<C-w>h", { noremap = true })
-vim.keymap.set("n", "<A-j>", "<C-w>j", { noremap = true })
-vim.keymap.set("n", "<A-k>", "<C-w>k", { noremap = true })
-vim.keymap.set("n", "<A-l>", "<C-w>l", { noremap = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true })
 
 -- yank to clipboard
 vim.cmd([[
