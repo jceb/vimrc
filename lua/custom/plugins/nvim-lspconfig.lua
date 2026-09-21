c8154c1e408cf02dc3ce0f7a5d44c411444845eb
Jan Christoph Ebersbach <jceb@e-jc.de>
2026-09-21 13:43:27 +0200

    chore(nvim-lspconfig): correct conflict with gr binding and backward character lookup via T → changed type definition binding to grD

--- lua/custom/plugins/nvim-lspconfig.lua
+++ lua/custom/plugins/nvim-lspconfig.lua
@@ -62,7 +62,7 @@ return {
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
         --  the definition of its *type*, not where it was *defined*.
-        map("grT", require("telescope.builtin").lsp_type_definitions, "[T]ype Definition")
+        map("grD", require("telescope.builtin").lsp_type_definitions, "[T]ype Definition")
 
         -- Fuzzy find all the symbols in your current document.
         --  Symbols are things like variables, functions, types, etc.
